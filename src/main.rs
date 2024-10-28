20f87c8d1216c788486e065d91b05ebffde988f5
bendeez <blaziken@MSI>
2024-10-27 23:54:39 -0500

    got rid of window because assets should be fixed size

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,10 @@ mod asset_loader;
 mod camera;
 mod movement;
 mod scene;
-mod window;
 mod yeti;
 use animation::AnimationPlugin;
 use camera::CameraPlugin;
 use scene::ScenePlugin;
-use window::WindowPlugin;
 use yeti::YetiPlugin;
 
 use bevy::prelude::*;
@@ -16,7 +14,6 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
-        .add_plugins(WindowPlugin)
         // prevents blurry sprites
         .add_plugins(CameraPlugin)
         .add_plugins(AnimationPlugin)
